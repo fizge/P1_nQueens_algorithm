f270c5e551888337d84c3cf99486688e47961418
fizge <fiz.garrido@udc.es>
2025-02-13 19:35:45 +0100

    create algorithm branch

--- Week1/obj/Debug/net9.0/Week1.AssemblyInfo.cs
+++ Week1/obj/Debug/net9.0/Week1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d54226ff8fcb41602199b9daa47345ebcb42a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5c00980ec9df2e81ec078395a5ea9f27cf3255")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
